d2f9833a6a6a1e274a321e28b984ddddc858a40a
Illia Goncharov <ilia.m.goncharov@gmail.com>
2024-12-15 01:53:18 +0300

    fix: api link in utils/api.js slash bug

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,6 +139,6 @@ class Api {
   }
   }
   // Замените на адрес вашего бэкенда
-const api = new Api('https://api.goncharov-mesto-plus.nomorepartiesco.ru/');
+const api = new Api('https://api.goncharov-mesto-plus.nomorepartiesco.ru');
 
   export default api;
